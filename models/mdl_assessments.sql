63a3e8bcd139116b0f3b5112d180d0d6fe56cd53
ksourabh45 <120578312+ksourabh45@users.noreply.github.com>
2024-03-13 18:02:20 +0530

    Update mdl_assessments.sql

--- models/mdl_assessments.sql
+++ models/mdl_assessments.sql
@@ -5,12 +5,12 @@ SELECT mdl_adaptiveassessment.timecreated,
     mdl_adaptiveassessment.name,
     mdl_adaptiveassessment.course,
     mdl_adaptiveassessment.id
-   FROM learninganalytics.mdl_adaptiveassessment
+   FROM learninganalytics.learninganalytics.mdl_adaptiveassessment
 UNION
  SELECT mdl_quiz.timecreated,
     mdl_quiz.timemodified,
     mdl_quiz.name,
     mdl_quiz.course,
     mdl_quiz.id
-   FROM learninganalytics.mdl_quiz
+   FROM learninganalytics.learninganalytics.mdl_quiz
 );

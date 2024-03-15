e045731bd7f2fb3ce793c9f7b8cf2b6247637942
ksourabh45 <120578312+ksourabh45@users.noreply.github.com>
2024-03-15 15:48:29 +0530

    Update Course.sql

--- models/Course.sql
+++ models/Course.sql
@@ -1,3 +1,3 @@
 --new values
-SELECT to_timestamp(enddate) as enddate, to_timestamp(timecreated) as timecreated , to_timestamp(timemodified) as timemodified, format, id, fullname, category, startdate, shortname
+SELECT id, fullname,shortname, category, format, to_timestamp(startdate) as startdate, to_timestamp(enddate) as enddate, to_timestamp(timecreated) as timecreated , to_timestamp(timemodified) as timemodified
 FROM learninganalytics.mdl_course
